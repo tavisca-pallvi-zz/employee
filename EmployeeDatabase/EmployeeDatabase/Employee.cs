a6212b05b5c397e302efd6b2bf1a34eb66abf58b
tavisca-pallvi <41461944+tavisca-pallvi@users.noreply.github.com>
2018-07-26 06:39:23 +0530

    Add files via upload

---
+++ EmployeeDatabase/EmployeeDatabase/Employee.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeDatabase
+{
+    class MyException : Exception
+    {
+        public MyException(string msg) : base(msg)
+        {
+
+        }
+
+    }
+
+
+
+    class Employee
+    {
+        private int _empId;
+        private string _empName;
+        private string _empQualification;
+      
+
+        public string Name {
+
+            get
+            {
+                return _empName;
+            }
+            set
+            {
+                _empName = value;
+            }
+
+
+        }
+        public int Id {
+
+            get
+            {
+                return _empId;
+            }
+            set
+            {
+                _empId = value;
+            }
+
+
+
+        }
+
+        public string qual
+        {
+
+            get
+            {
+                return _empQualification;
+            }
+            set
+            {
+                _empQualification = value;
+            }
+
+
+
+        }
+
+
+        public int CheckQualification()
+        {
+            if (_empQualification.Equals("BE") || _empQualification.Equals("BSE") || _empQualification.Equals("BCA"))
+            {
+
+
+                Console.WriteLine("Employee added under IT Department");
+            }
+
+           else if (_empQualification.Equals("BCom") || _empQualification.Equals("MCom") || _empQualification.Equals("CA"))
+
+
+                Console.WriteLine("Employee added under Account Department");
+            else
+            {
+
+                throw new MyException("Qualification can’t be empty");
+                
+            }
+            return 1;
+
+
+        }
+    }
+}
+    
+
+
+
+
+
+
+    
---
+++ EmployeeDatabase/EmployeeDatabase/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace EmployeeDatabase
+{
+    class Program
+    {
+
+        public delegate int MyDelegate();
+
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>();
+            int f = 1;
+            while (f == 1) {
+                Console.WriteLine("Do you want to add employee enter '1' else '0'");
+
+
+                int check = Int32.Parse(Console.ReadLine());
+                if (check == 0) {
+                    f = 0;
+                    Console.WriteLine("Application Exited");
+
+                    System.Environment.Exit(0);
+                    Console.ReadLine();
+                    return;
+                }
+
+                Employee emp = new Employee();
+
+                Console.WriteLine("Enter the id of employee");
+                int id = Int32.Parse(Console.ReadLine());
+                emp.Id = id;
+                Console.WriteLine("Enter the name of employee");
+                string name = Console.ReadLine();
+                emp.Name = name;
+                Console.WriteLine("Enter the qualification of employee");
+                string qual = Console.ReadLine();
+                emp.qual = qual;
+
+                employees.Add(emp);
+
+                Console.WriteLine(" employees succesfully added");
+                Console.WriteLine("Employee name is {0} \n Employee id is {1} \n Employee qualification is {2}", emp.Name, emp.Id, emp.qual);
+                //Properties Used
+                MyDelegate obj = new MyDelegate(emp.CheckQualification);
+              
+
+                try
+                {
+                    int q = obj();
+                    
+                }
+                catch (MyException ex)
+                {
+                    Console.WriteLine(ex.Message);
+
+                    using (StreamWriter writer = new StreamWriter("C:\\test.txt"))
+                    {
+                        writer.WriteLine(ex.Message);
+                    }
+
+                }
+
+                finally
+                {
+                    Console.WriteLine("\nExecution of the finally block after an unhandled\n");
+                            
+                }
+
+            }
+
+
+            Console.ReadLine();
+
+        }
+
+        
+    }
+
+}
